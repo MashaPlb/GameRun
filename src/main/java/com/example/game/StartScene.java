5d64e380ec8c7fab17b98979399e2d4e50ce2de6
afoniinaa <desember2404@gmail.com>
2024-04-30 01:29:03 +0500

    for Katy

---
+++ src/main/java/com/example/game/StartScene.java
@@ -0,0 +1,23 @@
+package com.example.game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class StartScene {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    public static boolean isSceneChanged = false;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
