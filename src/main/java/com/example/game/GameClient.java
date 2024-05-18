18a2c840271ba50274388b2a08245017ecc2db7b
Мария <masha2010ff@mail.ru>
2024-05-18 14:19:39 +0500

    18.05

--- src/main/java/com/example/game/GameClient.java
+++ src/main/java/com/example/game/GameClient.java
@@ -1,12 +1,14 @@
 package com.example.game;
 
+import javafx.application.Platform;
+
 import java.io.*;
 import java.net.*;
 
 public class GameClient {
     private Socket clientSocket;
     private ObjectOutputStream out;
-    private ObjectInputStream in;
+    private static ObjectInputStream in;
 
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
@@ -20,6 +22,27 @@ public class GameClient {
 
     public Object receiveMessage() throws IOException, ClassNotFoundException {
         return in.readObject();
+    }
+    public static void waitForStartSignal() throws IOException, ClassNotFoundException {
+        Object message;
+        while ((message = in.readObject()) != null) {
+            if ("START_GAME".equals(message.toString())) {
+                // Запуск игры
+                startGame();
+                break;
+            }
+        }
+    }
+
+    private static void startGame() {
+        // Реализуйте логику перехода на игровую сцену
+        Platform.runLater(() -> {
+            try {
+                HelloApplication.changeScene("game-scene.fxml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void stopConnection() throws IOException {
